1ef523015d98445a6ec5b54a2ba651f4c8fde373
Hugo Patrício <hugopatricio51@gmail.com>
2021-07-02 12:53:50 -0300

    adicionando mais rotas

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@ const express = require('express')
 const routes = express.Router()
 
 
-routes.get('/', (req,res) => {
-    return res.render("index")
-})
+routes.get('/', (req,res) => res.render("index"))
+routes.get('/room', (req,res) => res.render('room'))
+routes.get('/create-pass', (req,res) => res.render('create-pass'))
+
+
+
 
 
 module.exports = routes
